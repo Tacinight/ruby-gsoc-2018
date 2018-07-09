060143ec2c56adb8cfd4358a515003479606ca3f
tacinight <tacingiht@gmail.com>
2018-07-09 21:52:54 +0800

    remove experimental sciptes
    Signed-off-by: tacinight <tacingiht@gmail.com>

--- 10times_yarv.rb
+++
@@ -1,6 +0,0 @@
-code = <<END
-10.times do |n|
-  puts n
-end
-END
-puts RubyVM::InstructionSequence.compile(code).disasm
--- complex_formula_yarv.rb
+++
@@ -1,7 +0,0 @@
-code = <<END
-def complex_formula(a, b, *args, c)
-  a + b + args.size + c
-end
-END
-
-puts RubyVM::InstructionSequence.compile(code).disasm
